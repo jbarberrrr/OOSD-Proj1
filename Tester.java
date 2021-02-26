54a94512eb478774dd15af1243089435a20581ec
Joshua Barber <58489546+jbarberrrr@users.noreply.github.com>
2021-02-26 17:26:14 -0600

    Add files via upload

--- Tester.java
+++ Tester.java
@@ -11,18 +11,20 @@ public class Tester {
 
 	public static void main(String[] args) {
 		
-		//track_id,artist_name,genre,track_title, album_title,year_created,artist_longitude
-		
-		// creates a song
+		// creates a pre-filled song
+		System.out.println("s1:");
 		Song s1 = new Song(1,"Luke Combs","Country","Beautiful Crazy","idk",0,0);
 		System.out.println(s1);
 		
-		// creates a song with default constructor then adds some data
+		// creates a null song with default constructor then adds some data to it
+		System.out.println("\ns2: (nothing added)");
 		Song s2 = new Song();
 		System.out.println(s2);
 		s2.setTrackID(3);
 		s2.setArtistName("Luke Bryan");
+		s2.setTrackTitle("Play It Again");
 		s2.setGenre("Country");
+		System.out.println("s2: (after edit)");
 		System.out.println(s2);
 		System.out.println();
 	
@@ -34,28 +36,48 @@ public class Tester {
 		Country.addSong(s2);
 		System.out.println("After adding songs:\n" + Country);	
 		
+		// removes song and displays updated playlist
+		Country.removeSong("Beautiful Crazy");
+		System.out.println("After removing songs:\n" + Country);	
+		
+		// testing the search song method (single input/single outputs)
+		Country.searchSong("Play It Again");
+		
+		// testing multiple title seach (multiple input/multiple outputs)
+		System.out.println("\nSearch for multiple songs from Country");
+		Country.searchSongs("TestDummy1", "TestDummy3", "RandomTitleThatsNotInPlaylist");
+		
+		// testing song seach with extra inputs (multiple input/single outputs)
+		System.out.println("\nSearch for a specific song from Country");
+		Country.searchSpecificSong("Luke Bryan", "Country", "Play It Again");
+		
+		// testing genre seach (single input/multiple outputs)
+		Song s4 = new Song(-1,"-1","Pop","TestDummy1","idk",0,0);
+		Song s5 = new Song(-1,"-1","Pop","TestDummy2","idk",0,0);
+		Song s6 = new Song(-1,"-1","Pop","TestDummy3","idk",0,0);
+		Song s7 = new Song(-1,"-1","Pop","TestDummy4","idk",0,0);
+		Country.addSong(s4);
+		Country.addSong(s5);
+		Country.addSong(s6);
+		Country.addSong(s7);
+		System.out.println("\n");
+		System.out.println(Country);
+		System.out.println("Search for 'Pop' Genre from Country");
+		Country.searchGenre("Pop");
+		
 		// test creating playlist from textfile
 		Playlist TextFile = new Playlist("TEXTFILE","./ProjPackage/finalTracks.csv");
-		System.out.println("\ntest reading text file");
-		// line below writes all of them, takes up entire console so I keep it commented
-		//System.out.println(TextFile);
-		//TextFile.addSong(s3);	
+		// line below prints the whole finalTracks.csv so I leave it commented out
+		// System.out.println(TextFile);
+		// TextFile.addSong(s1);
 		
-		/*
-		Instructor i4 = new Instructor("mark","987","m@uca.edu");
-		csci3381.addInstructor(i4);		
+		// adds new song to TextFile and searches for it, returning it
+		//Song s3 = new Song(1,"Riley Green","Country","Georgia Time","idk",0,0);
+		//TextFile.addSong(s3);
+		//TextFile.searchSong("Georgia Time");
 		
-		System.out.println("\ntest combined data from file and data added manually");
-		System.out.println(csci3381);		
-		csci3381.writeFile("./classroster/dataWriteTest.txt");
-		*/
-		
-		//Country.writeFile("./classroster/dataWriteTest.txt");
-		
-		//Playlist testWrite = new Playlist("Test Write","./classroster/dataWriteTest.txt");
-		//System.out.println("\ntest reading just created file");
-		//System.out.println(testWrite);
-		
+		//System.out.println("\n idk ");
+		//System.out.println(Country);		
+		//Country.writeFile("./ProjPackage/Country.txt");
 	}
-
 }
